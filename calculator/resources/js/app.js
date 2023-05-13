92d282e4f8ec5dc55ac5082832ab97242d8f4e88
makhelij <m.akhelij@gmail.com>
2023-05-13 22:12:44 +0100

    tailwind and vue implementation

--- calculator/resources/js/app.js
+++ calculator/resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import Calculator from './components/Calculator.vue';
+
+createApp({})
+    .component('Calculator', Calculator)
+    .mount('#app')
